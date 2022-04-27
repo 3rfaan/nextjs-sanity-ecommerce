77489fd635c8aca0228b81d29f03fa5cb00828d3
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-04-27 15:07:01 +0200

    NextJS/Sanity Ecommerce Shop

---
+++ components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">MERN Shop</Link>
+      </p>
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => {
+          setShowCart((prevShowCart) => !prevShowCart);
+        }}
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
